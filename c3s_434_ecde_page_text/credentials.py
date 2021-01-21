d263b74c4cd1998e390d0609a40160af67ab3d7f
Jonathan Haigh <jhaigh0@sci5.jasmin.ac.uk>
2021-01-21 15:20:10 +0000

    created basic file structure, copied over credentials.py

---
+++ c3s_434_ecde_page_text/credentials.py
@@ -0,0 +1,40 @@
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+import os
+
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+
+if not os.path.isfile(CLIENT_SECRET_FILE):
+    raise Exception(f'You must create file {CLIENT_SECRET_FILE} for this to work. '
+                     'See instructions at: https://developers.google.com/sheets/api/quickstart/python')
+
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    
+    return credentials
